cd3d7fa827b8e5d3fbad09acc84b5c09f80d1fde
RS-lalit-tyagi <lalit.tyagi@remotestate.com>
2021-07-07 23:20:02 +0530

    Update cloudinaryconfig.js

--- config/cloudinaryconfig.js
+++ config/cloudinaryconfig.js
@@ -2,10 +2,10 @@ const { config, uploader } = require('cloudinary').v2;
 // const { CloudinaryStorage } = require('multer-storage-cloudinary')
 
 const coloudinaryConfig = config({
-    cloud_name: 'lucifer07',
-    api_key: '695741565517692',
-    api_secret: '8SDKYxt4PcwrKY_A9ZladGXwvR0'
-});
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,l̥
+})
 
 // const storage = new CloudinaryStorage({
 //     cloudinary,
